24f399df6e6552db6f1515abbfa879181edc296e
Rijk van Zanten <rijkvanzanten@me.com>
2017-03-06 16:07:20 +0100

    :fire: remove throttle function

--- app/public/script.js
+++ app/public/script.js
@@ -1,50 +1,6 @@
 /* global window, navigator, document, MediaStreamTrack */
 
 (function() {
-  /**
-   * Underscores throttle function
-   */
-  function throttle(func, wait, options) {
-    var timeout, context, args, result;
-    var previous = 0;
-    if (!options) options = {};
-
-    var later = function() {
-      previous = options.leading === false ? 0 : Date.now();
-      timeout = null;
-      result = func.apply(context, args);
-      if (!timeout) context = args = null;
-    };
-
-    var throttled = function() {
-      var now = Date.now();
-      if (!previous && options.leading === false) previous = now;
-      var remaining = wait - (now - previous);
-      context = this;
-      args = arguments;
-      if (remaining <= 0 || remaining > wait) {
-        if (timeout) {
-          clearTimeout(timeout);
-          timeout = null;
-        }
-        previous = now;
-        result = func.apply(context, args);
-        if (!timeout) context = args = null;
-      } else if (!timeout && options.trailing !== false) {
-        timeout = setTimeout(later, remaining);
-      }
-      return result;
-    };
-
-    throttled.cancel = function() {
-      clearTimeout(timeout);
-      previous = 0;
-      timeout = context = args = null;
-    };
-
-    return throttled;
-  }
-
   const app = {
     init() {
       compass.init();
@@ -54,9 +10,9 @@
 
   const compass = {
     init() {
-      window.addEventListener('deviceorientationabsolute', throttle(event => {
+      window.addEventListener('deviceorientationabsolute', event => {
         this.value = Math.floor(360 - event.alpha);
-      }, 10));
+      });
     },
     value: null
   };
