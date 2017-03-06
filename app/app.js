2ce2d9a4b0e6aa59d6f7e9eb09abc95792c91ab0
Rijk van Zanten <rijkvanzanten@me.com>
2017-03-06 12:52:27 +0100

    :sparkles: add public static assets folder

--- app/app.js
+++ app/app.js
@@ -5,6 +5,11 @@ const app = express();
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public'), {
+  index: false,
+  maxage: 604800000,
+}));
+
 app.get('*', (req, res) => {
   res.render('index');
 });
