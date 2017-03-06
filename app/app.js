e04f4bfa307c16ebd486dab097fe03356276c7ad
Rijk van Zanten <rijkvanzanten@me.com>
2017-03-06 19:28:05 +0100

    :sparkles: add locality lookup route

--- app/app.js
+++ app/app.js
@@ -1,6 +1,9 @@
 const path = require('path');
+const fetch = require('node-fetch');
 const express = require('express');
 const app = express();
+
+const apiKeys = require('./api-keys');
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
@@ -9,6 +12,22 @@ app.use(express.static(path.join(__dirname, 'public'), {
   index: false,
   maxage: 604800000,
 }));
+
+app.get('/locality/:lat/:lon', (req, res) => {
+  const { lat, lon } = req.params;
+  fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${apiKeys.google}`)
+    .then(records => records.json())
+    .then(records => {
+      const components = records.results[0].address_components;
+      const localities = components.filter(c => c.types.includes('locality'));
+      const locality = localities.length ? localities[0].long_name.toLowerCase() : '';
+
+      const postalCodes = components.filter(c => c.types.includes('postal_code'));
+      const postalCode = postalCodes.length ? postalCodes[0].long_name.toLowerCase().substr(0, 4) : '';
+
+      res.send({ locality, postalCode });
+    });
+});
 
 app.get('*', (req, res) => res.render('index'));
 
