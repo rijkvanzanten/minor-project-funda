da3d8e7a0e9f9808ad0c523d3cbe890fd53581de
Rijk van Zanten <rijkvanzanten@me.com>
2017-03-06 12:13:54 +0100

    :tada: setup basic express/ejs app

---
+++ app/app.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+app.get('*', (req, res) => {
+  res.render('index');
+});
+
+module.exports = app;
